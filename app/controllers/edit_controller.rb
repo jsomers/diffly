cf3682747b5a80d1ab4aeb0670843212150cb765
James Somers <jsomers@gmail.com>
2011-03-11 00:12:55 -0800

    Workaround?

--- app/controllers/edit_controller.rb
+++ app/controllers/edit_controller.rb
@@ -9,8 +9,7 @@ class EditController < ApplicationController
   def make_blob
     content = params[:content]
     hash = Digest::MD5.hexdigest(content).first(7)
-    b = Blob.new(:hash => hash, :content => content)
-    b.save
+    begin Blob.create(:hash => hash, :content => content) rescue p "oops" end
     render :text => hash
   end
 end
