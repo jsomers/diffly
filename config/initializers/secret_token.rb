95ab1ece897aaed0f6e581a421b6ccabed731ad6
James Somers <jsomers@gmail.com>
2011-03-10 23:26:41 -0800

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diffrails::Application.config.secret_token = '052973bf9937f365687a5a7ed473768270cd1c38de9c2c9c496a11056fb99b59e53c3b9866c7c49b9e327ce32c79141215b4e4dcc9f14bca88fb06df9d7a2acc'
